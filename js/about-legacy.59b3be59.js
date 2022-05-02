5895cc5e4f419bd2734fa383c2e1fca8c68257bc
Omowunmi Ekun <omowunmiekun1@gmail.com>
2022-05-02 11:38:49 +0100

    deploy

---
+++ js/about-legacy.59b3be59.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktest_project"]=self["webpackChunktest_project"]||[]).push([[443],{4613:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],r=n(3736),a={},c=(0,r.Z)(a,s,u,!1,null,null,null),l=c.exports}}]);
+//# sourceMappingURL=about-legacy.59b3be59.js.map
